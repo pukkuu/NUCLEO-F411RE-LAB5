354fb384771ab55c7f3d98c8b7347d6101a00cfc
pukkuu <78376947+pukkuu@users.noreply.github.com>
2023-05-11 15:50:40 +0700

    final

---
+++ Core/Src/main.c
@@ -0,0 +1,433 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "stdio.h"
+#include "string.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+UART_HandleTypeDef huart2;
+DMA_HandleTypeDef hdma_usart2_rx;
+
+/* USER CODE BEGIN PV */
+uint8_t TxBuffer[50];
+uint8_t RxBuffer[10];
+const uint8_t ClrBuffer[50] = {0};
+
+uint8_t State = '\0';
+
+GPIO_PinState LEDEnable = 1;
+uint8_t LEDfreq = 1;
+
+GPIO_PinState ButtonCurr = 1;
+GPIO_PinState ButtonPrev = 0;
+
+uint32_t BlinkTime;
+uint32_t ButtonTime;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_DMA_Init(void);
+static void MX_USART2_UART_Init(void);
+/* USER CODE BEGIN PFP */
+void Menu(uint8_t num);
+void LEDMenu(uint8_t Pos);
+void ButtonMenu(uint8_t Pos);
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+  HAL_UART_Receive_DMA(&huart2, RxBuffer, 10);
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+
+  BlinkTime = HAL_GetTick() + (500 / LEDfreq);
+  ButtonTime = HAL_GetTick() + 20;
+  uint8_t BufferPrevPos = ((2 * huart2.RxXferSize) - huart2.hdmarx->Instance->NDTR - 1) % huart2.RxXferSize;
+
+  Menu(0);
+
+  while (1)
+  {
+	  uint8_t BufferPos = ((2 * huart2.RxXferSize) - huart2.hdmarx->Instance->NDTR - 1) % huart2.RxXferSize;
+
+	  if (BufferPos != BufferPrevPos) {
+		  sprintf((char *) TxBuffer, "Receive Command: %c\r\n", RxBuffer[BufferPos]);
+		  HAL_UART_Transmit(&huart2, TxBuffer, sizeof(TxBuffer), HAL_MAX_DELAY);
+		  memcpy(TxBuffer, ClrBuffer, 50);
+	  }
+
+	  switch (State)
+	  {
+		case '0':
+			LEDMenu(BufferPos);
+			break;
+		case '1':
+			ButtonMenu(BufferPos);
+			break;
+		case 'x':
+			Menu(0);
+			RxBuffer[BufferPos] = '\0';
+		default:
+			State = RxBuffer[BufferPos];
+			switch (State) {
+			case '0':
+				Menu(1);
+				break;
+			case '1':
+				Menu(2);
+				if (HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin)) ButtonPrev = 0;
+				else ButtonPrev = 1;
+				break;
+			case 'x':
+				break;
+			case '\0':
+				break;
+			default:
+				Menu(3);
+			}
+	  }
+	  RxBuffer[BufferPos] = '\0';
+	  BufferPrevPos = BufferPos;
+
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+
+	  if (HAL_GetTick() >= BlinkTime)
+	  {
+		  if (LEDEnable) HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+		  BlinkTime += (500 / LEDfreq);
+	  }
+
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 100;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 4;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 57600;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void)
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA1_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA1_Stream5_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Stream5_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Stream5_IRQn);
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+void Menu(uint8_t num)
+{
+	if (num == 0)
+	{
+		HAL_UART_Transmit(&huart2, (uint8_t *) "\r\n--------------------------------------------------\r\n", strlen("\r\n--------------------------------------------------\r\n"), HAL_MAX_DELAY);
+		HAL_UART_Transmit(&huart2, (uint8_t *) "Main Menu\r\n", strlen("Main Menu\r\n"), HAL_MAX_DELAY);
+		HAL_UART_Transmit(&huart2, (uint8_t *) "Press 0 to Enter LED Control Mode\r\n", strlen("Press 0 to Enter LED Control Mode\r\n"), HAL_MAX_DELAY);
+		HAL_UART_Transmit(&huart2, (uint8_t *) "Press 1 to Enter Button Status Mode\r\n", strlen("Press 1 to Enter Button Status Mode\r\n"), HAL_MAX_DELAY);
+		HAL_UART_Transmit(&huart2, (uint8_t *) "--------------------------------------------------\n\r\n", strlen("--------------------------------------------------\n\r\n"), HAL_MAX_DELAY);
+	}
+	else if (num == 1)
+	{
+		HAL_UART_Transmit(&huart2, (uint8_t *) "\r\n--------------------------------------------------\r\n", strlen("\r\n--------------------------------------------------\r\n"), HAL_MAX_DELAY);
+		HAL_UART_Transmit(&huart2, (uint8_t *) "LED Control Mode\r\n", strlen("LED Control Mode\r\n"), HAL_MAX_DELAY);
+		HAL_UART_Transmit(&huart2, (uint8_t *) "Press a to Speed Up 1Hz\r\n", strlen("Press a to Speed Up 1Hz\r\n"), HAL_MAX_DELAY);
+		HAL_UART_Transmit(&huart2, (uint8_t *) "Press s to Speed Down 1Hz\r\n", strlen("Press a to Speed Down 1Hz\r\n"), HAL_MAX_DELAY);
+		HAL_UART_Transmit(&huart2, (uint8_t *) "Press d to Turn On/Off\r\n", strlen("Press d to Turn On/Off\r\n"), HAL_MAX_DELAY);
+		HAL_UART_Transmit(&huart2, (uint8_t *) "Press x to Return To Menu\r\n", strlen("Press x to Return To Menu\r\n"), HAL_MAX_DELAY);
+		HAL_UART_Transmit(&huart2, (uint8_t *) "--------------------------------------------------\n\r\n", strlen("--------------------------------------------------\n\r\n"), HAL_MAX_DELAY);
+	}
+	else if (num == 2)
+	{
+		HAL_UART_Transmit(&huart2, (uint8_t *) "\r\n--------------------------------------------------\r\n", strlen("\r\n--------------------------------------------------\r\n"), HAL_MAX_DELAY);
+		HAL_UART_Transmit(&huart2, (uint8_t *) "Button Status Mode\r\n", strlen("Button Status Mode\r\n"), HAL_MAX_DELAY);
+		HAL_UART_Transmit(&huart2, (uint8_t *) "Press Blue button and Observe Status\r\n", strlen("Press Blue button and Observe Status\r\n"), HAL_MAX_DELAY);
+		HAL_UART_Transmit(&huart2, (uint8_t *) "Press x to Return To Menu\r\n", strlen("Press x to Return To Menu\r\n"), HAL_MAX_DELAY);
+		HAL_UART_Transmit(&huart2, (uint8_t *) "--------------------------------------------------\n\r\n", strlen("\r\n--------------------------------------------------\n\r\n"), HAL_MAX_DELAY);
+	}
+	else if (num == 3)
+	{
+		HAL_UART_Transmit(&huart2, (uint8_t *) "Error Command\r\n", strlen("Error Command\r\n"), HAL_MAX_DELAY);
+	}
+}
+
+void LEDMenu(uint8_t Pos)
+{
+	switch(RxBuffer[Pos]) {
+	case 'a':
+		LEDEnable = 1;
+		if (LEDfreq < 255) LEDfreq += 1;
+		else {
+			HAL_UART_Transmit(&huart2, (uint8_t *) "Warnning Reach Frequency Limit\r\n", strlen("Warnning Reach Frequency Limit\r\n"), HAL_MAX_DELAY);
+		}
+		sprintf((char *) TxBuffer, "LED Blink Rate: %d Hz\r\n", LEDfreq);
+		HAL_UART_Transmit(&huart2, TxBuffer, sizeof(TxBuffer), HAL_MAX_DELAY);
+		memcpy(TxBuffer, ClrBuffer, 50);
+		break;
+	case 's':
+		LEDEnable = 1;
+		if (LEDfreq > 1) LEDfreq -= 1;
+		else {
+			HAL_UART_Transmit(&huart2, (uint8_t *) "Warnning Reach Frequency Limit\r\n", strlen("Warnning Reach Frequency Limit\r\n"), HAL_MAX_DELAY);
+		}
+		sprintf((char *) TxBuffer, "LED Blink Rate: %d Hz\r\n", LEDfreq);
+		HAL_UART_Transmit(&huart2, TxBuffer, sizeof(TxBuffer), HAL_MAX_DELAY);
+		memcpy(TxBuffer, ClrBuffer, 50);
+		break;
+	case 'd':
+		LEDEnable = 0;
+		HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+		if (HAL_GPIO_ReadPin(LD2_GPIO_Port, LD2_Pin) == 1) HAL_UART_Transmit(&huart2, (uint8_t *) "LED Status: On\r\n", strlen("LED Status: On\r\n"), HAL_MAX_DELAY);
+		else HAL_UART_Transmit(&huart2, (uint8_t *) "LED Status: Off\r\n", strlen("LED Status: Off\r\n"), HAL_MAX_DELAY);
+		break;
+	case 'x':
+		State = 'x';
+		break;
+	case '\0':
+		break;
+	default:
+		Menu(3);
+	}
+}
+
+void ButtonMenu(uint8_t Pos)
+{
+	switch(RxBuffer[Pos]) {
+	case 'x':
+		State = 'x';
+		break;
+	case '\0':
+		if (HAL_GetTick() >= ButtonTime) {
+			ButtonCurr = HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin);
+			if (!ButtonCurr && ButtonPrev) HAL_UART_Transmit(&huart2, (uint8_t *) "Button Press\r\n", strlen("Button Press\r\n"), HAL_MAX_DELAY);
+			else if (ButtonCurr && !ButtonPrev) HAL_UART_Transmit(&huart2, (uint8_t *) "Button UnPress\r\n", strlen("Button UnPress\r\n"), HAL_MAX_DELAY);
+			ButtonPrev = ButtonCurr;
+			ButtonTime += 20;
+		}
+		break;
+	default:
+		Menu(3);
+	}
+}
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
